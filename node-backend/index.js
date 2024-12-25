1902902231c9d3fb705ba37fac14130889b7093f
pihu <pihuc250@gmail.com>
2024-12-25 19:44:19 +0530

    11th-commit

--- node-backend/index.js
+++ node-backend/index.js
@@ -53,8 +53,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // MongoDB connection
 // const mongoURL =  'mongodb+srv://helloworld:Ho6oGu69Zd1yEygz@cluster0.yduksfk.mongodb.net/Bookrecycle?retryWrites=true&w=majority';
-const mongoURL =  'mongodb://localhost:27017/Bookrecycle';
-// const mongoURL =  'mongodb://127.0.0.1:27017/Bookrecycle';
+// const mongoURL =  'mongodb://localhost:27017/Bookrecycle';
+const mongoURL =  'mongodb://127.0.0.1:27017/Bookrecycle';
 mongoose.connect(mongoURL, {})
     .then(() => console.log('Connected to MongoDB server'))
     .catch((error) => {
