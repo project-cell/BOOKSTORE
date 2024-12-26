023db992fe6b7bafac6a8bf41b3d38385aaf9214
pihu <pihuc250@gmail.com>
2024-12-26 11:37:26 +0530

    21th-commit

--- node-backend/index.js
+++ node-backend/index.js
@@ -46,7 +46,7 @@ const port = 3000;
         //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
         //     credentials: true // If you need to allow cookies or authentication
         // }));
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
